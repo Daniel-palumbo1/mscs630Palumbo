e02e9288b3b86d920e281cecae4d902d478ed5cb
danielgpalumbo <Danielgpalumbo@gmail.com>
2021-02-27 23:58:39 -0500

    Adding partial work done for lab1 TODO: comments/testing

---
+++ lab/lab1/Driver_lab1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Driver_lab1{
+
+    static final int CORRECTION_FACTOR = 10;
+    static final int SPACE_NUM = 26;
+    static final String FILE_NAME = "./input/input1.txt";
+    
+    public static int[] str2int(String plainText){
+        int[] output = new int[plainText.length()];
+        char[] outputAsChar = plainText.toCharArray();
+        int index = 0;
+        for(char n: outputAsChar){
+            if(Character.getNumericValue(n) != -1){
+                output[index] = Character.getNumericValue(n) - CORRECTION_FACTOR;
+            }
+            else{
+                output[index] = SPACE_NUM;
+            }
+            index++;
+        }
+        return output;
+    }
+
+
+    public static void main(String[] arg){
+        try{
+            File file = new File(FILE_NAME);
+            Scanner in = new Scanner(file);
+            while(in.hasNextLine()){
+                int[] out = str2int(in.nextLine());
+                for(int n: out)
+                    System.out.print(n + " ");
+                System.out.println();
+            }
+            in.close();
+        }catch(Exception e){
+            System.out.println("Something went wrong most likely with FILE_NAME variable");
+            System.out.println(e);
+        }
+    }
+}

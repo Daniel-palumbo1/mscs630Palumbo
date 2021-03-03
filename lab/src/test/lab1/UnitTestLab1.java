e7f3dc6e7a1e11a8d35a5069ea9372c1c9998ed7
danielgpalumbo <Danielgpalumbo@gmail.com>
2021-03-03 15:19:14 -0500

    Got unit testing working TODO: comments

---
+++ lab/src/test/lab1/UnitTestLab1.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+import org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitTestLab1{
+    @Test
+    void test1(){
+
+    }
+}

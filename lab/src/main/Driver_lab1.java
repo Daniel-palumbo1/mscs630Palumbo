7e259ac3fe38592c9ea36182fe9042d1561ccc3b
danielgpalumbo <Danielgpalumbo@gmail.com>
2021-03-06 17:56:25 -0500

    added comments COMPLETE

--- lab/src/main/Driver_lab1.java
+++ lab/src/main/Driver_lab1.java
@@ -1,3 +1,13 @@
+/**
+ * file: Driver_lab1.java
+ * author: Daniel Palumbo
+ * course: MSCS_630_711_21S
+ * assignment: lab 1
+ * due date: 6 March, 2021
+ *
+ * This file contains a driver main method that takes in a file and prints
+ * the file as integers corresponding to the characters of the file.
+ */
 package main;
 
 import java.io.File;
@@ -5,14 +15,25 @@ import java.util.Scanner;
 
 public class Driver_lab1{
 
-    static final int CORRECTION_FACTOR = 10;
+    static final int CORRECTION_FACTOR = 10; //adjustment value for getNumericValue a = 10
     static final int SPACE_NUM = 26;
     static final String FILE_NAME = "src/test/in/input.10.in";
-    
+
+    /**
+     * str2int
+     *
+     * This method takes a String of plain text, converts all characters in the
+     * string to integers using Character.getNumericValue() and then returns an array
+     * containing those integer values. a-z = 0-25 and space = 26.
+     *
+     * @param plainText String containing any string.
+     * @return output int[] containing characters as integers.
+     */
     public static int[] str2int(String plainText){
         int[] output = new int[plainText.length()];
         char[] outputAsChar = plainText.toCharArray();
         int index = 0;
+
         for(char n: outputAsChar){
             if(Character.getNumericValue(n) != -1){
                 output[index] = Character.getNumericValue(n) - CORRECTION_FACTOR;
@@ -30,8 +51,10 @@ public class Driver_lab1{
         try{
             File file = new File(FILE_NAME);
             Scanner in = new Scanner(file);
+
             while(in.hasNextLine()){
                 int[] out = str2int(in.nextLine());
+
                 for(int n: out)
                     System.out.print(n + " ");
                 System.out.println();
